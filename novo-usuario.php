92d5e93aa8da85b2c2e00f69096ef3ee564e0135
caiodds <rhyanteixeira04@gmail.com>
2022-12-01 06:56:24 -0300

    TCC

--- novo-usuario.php
+++ novo-usuario.php
@@ -8,19 +8,19 @@
     <input type="hidden" name="acao" value="cadastrar">
 <div class="mb-3">
     <label for="">Nome</label>
-    <input type="text" name="nome" class="form-control" placeholder="Nome" require>
+    <input type="text" name="nome" class="form-control" placeholder="Nome" required>
 </div>
 <div class="mb-3">
     <label for="">E-mail</label>
-    <input type="email" name="email" class="form-control" placeholder="E-mail" require>
+    <input type="email" name="email" class="form-control" placeholder="E-mail" required>
 </div>
 <div class="mb-3">
     <label for="">Senha</label>
-    <input type="password" name="senha" class="form-control" placeholder="Senha" require>
+    <input type="password" name="senha" class="form-control" placeholder="Senha" required>
 </div>
 <div class="mb-3">
     <label for="">Data Nascimento</label>
-    <input type="date" name="data_nasc" class="form-control" placeholder="Nome" require>
+    <input type="date" name="data_nasc" class="form-control" placeholder="Nome" required>
 </div>
 <div class="mb-3">
     <button type="submit" class="btn btn-primary" >Enviar</button>
